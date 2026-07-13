869cff2da5e6a160ed17a1166a85ed5ae9b8bf7b
turn-world <whdydwns0604@gmail.com>
2026-07-13 18:39:27 +0900

    settings : 곡 선택 & 속도 관리 구현

---
+++ src/settings/__init__.py
@@ -0,0 +1,19 @@
+from .settings import (
+    Settings,
+    Song,
+    SPEED_PRESETS,
+    MIN_SPEED,
+    MAX_SPEED,
+    INPUT_SOURCES,
+    DEFAULT_INPUT_SOURCE,
+)
+
+__all__ = [
+    "Settings",
+    "Song",
+    "SPEED_PRESETS",
+    "MIN_SPEED",
+    "MAX_SPEED",
+    "INPUT_SOURCES",
+    "DEFAULT_INPUT_SOURCE",
+]
